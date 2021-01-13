5f5ba8601887f79b75543c92f9f1abe4d3bdffd6
Lunush <76045303+lunush@users.noreply.github.com>
2021-01-13 15:30:15 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Background from './Background.svelte';
+</script>
+
+<Background>
+  <h1>Hi</h1>
+</Background>
+
+<style>
+  :global(html),
+  :global(body) {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  h1 {
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
---
+++ src/Background.svelte
@@ -0,0 +1,92 @@
+<script>
+const getRandomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+const generateStarsPosition = (number) => {
+  let result = ''
+
+  for(let i = 0; i < number; i++) {
+    result += ` #222 ${getRandomNumber(0, 3000)}px ${getRandomNumber(0, 3000)}px,`
+  }
+
+  return result.substring(0, result.length - 1)
+}
+
+const [stars1, stars2, stars3] = [generateStarsPosition(700),
+  generateStarsPosition(400), generateStarsPosition(100)]
+</script>
+
+<main>
+  <div class="background">
+    <div id="stars1" style="box-shadow: {stars1}"/>
+    <div id="stars2" style="box-shadow: {stars2}"/>
+    <div id="stars3" style="box-shadow: {stars3}"/>
+  </div>
+  <slot></slot>
+</main>
+
+<style>
+  main {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  .background {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+    left: 0%;
+    top: 0%;
+    position: absolute;
+    -webkit-transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    z-index: -1;
+    transform: translateZ(-15) scale(15);
+    background: radial-gradient(ellipse at center, #ddd 0%, #fff 100%)
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  #stars1 {
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    background: transparent;
+    animation: animStar 800s linear infinite;
+  }
+
+  #stars2 {
+    width: 2px;
+    height: 2px;
+    overflow: hidden;
+    background: transparent;
+    animation: animStar 500s linear infinite;
+  }
+
+  #stars3 {
+    width: 3px;
+    height: 3px;
+    overflow: hidden;
+    background: transparent;
+    animation: animStar 200s linear infinite;
+  }
+
+  @keyframes animStar {
+    from {
+      transform: translateY(0px);
+    }
+    to {
+      transform: translateY(-2000px);
+    }
+  }
+</style>
