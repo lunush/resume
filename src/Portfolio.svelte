8e2d2b17269524888ade4ca19dab78e3d97aba75
Lunush <76045303+lunush@users.noreply.github.com>
2021-01-14 20:57:01 +0300

    Update Portfolio

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -3,11 +3,16 @@
 </script>
 
 <Section>
-  <div
-    class="flex flex-wrap content-center justify-around w-screen h-screen text-center"
-  >
-    <h1 class="text-5xl font-thin text-gray-900 uppercase">Posty</h1>
-    <h1 class="text-5xl font-thin text-gray-900 uppercase">Bixov</h1>
-    <h1 class="text-5xl font-thin text-gray-900 uppercase">Notes</h1>
+  <div class="flex flex-col content-center justify-center w-screen h-screen">
+    <h1 class="mb-12 text-3xl font-thin text-center text-gray-900 uppercase">
+      Some of my works
+    </h1>
+    <div
+      class="flex flex-wrap content-center justify-around w-screen text-center"
+    >
+      <h1 class="text-5xl font-thin text-gray-900 uppercase">Posty</h1>
+      <h1 class="text-5xl font-thin text-gray-900 uppercase">Bixov</h1>
+      <h1 class="text-5xl font-thin text-gray-900 uppercase">Notes</h1>
+    </div>
   </div>
 </Section>
