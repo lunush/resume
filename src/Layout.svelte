1dd98204b7e09348ead615c69bf5f9919fa95a52
Lunush <76045303+lunush@users.noreply.github.com>
2021-01-17 21:35:06 +0300

    Add smooth appearance on load

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   import { Router, Link, Route, link } from 'svelte-routing';
   import Background from './Background.svelte';
+
+  let visible = false;
+
+  setTimeout(() => {
+    visible = true;
+  }, 0);
 </script>
 
 <div class="w-screen h-screen overflow-hidden">
   <Background>
-  <div class="absolute flex content-center justify-between w-full h-8">
-    <Link to="/portfolio" class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900">Portfolio</Link>
-    <Link to="/about" class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900">About</Link>
-  </div>
-    <slot></slot>
+    {#if visible}
+      <div
+        transition:fly="{{ y: -50, duration: 1500 }}"
+        class="w-screen h-screen overflow-hidden"
+      >
+        <div class="absolute flex content-center justify-between w-full h-8">
+          <Link
+            to="/portfolio"
+            class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
+          >Portfolio</Link
+          >
+          <Link
+            to="/about"
+            class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
+          >About</Link
+          >
+        </div>
+        <slot />
+      </div>
+    {/if}
   </Background>
 </div>
