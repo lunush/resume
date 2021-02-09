20b011444fa7b924ca4fbf79f84906d3ffd1c5f3
Lunush <76045303+lunush@users.noreply.github.com>
2021-02-09 20:29:13 +0300

    Remove parallax stars background

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -1,7 +1,6 @@
 <script>
   import { fade } from 'svelte/transition';
   import { Link } from 'svelte-routing';
-  import Background from './Background.svelte';
 
   export let setFloatingVideoVisibility;
   let visible = false;
@@ -16,22 +15,20 @@
     transition:fade="{{ duration: 1000 }}"
     class="w-screen h-screen overflow-hidden"
   >
-    <Background>
-      <div class="absolute z-10 flex content-center justify-between w-full h-8">
-        <Link
-          to="/portfolio"
-          on:click="{() => setFloatingVideoVisibility(true)}"
-          class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
+    <div class="absolute z-10 flex content-center justify-between w-full h-8">
+      <Link
+        to="/portfolio"
+        on:click="{() => setFloatingVideoVisibility(true)}"
+        class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
         >Portfolio</Link
-        >
-        <Link
-          to="/about"
-          on:click="{() => setFloatingVideoVisibility(true)}"
-          class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
+      >
+      <Link
+        to="/about"
+        on:click="{() => setFloatingVideoVisibility(true)}"
+        class="m-4 text-xl font-light text-gray-900 no-underline hover:no-underline hover:text-gray-500 visited:text-gray-900"
         >About</Link
-        >
-      </div>
-      <slot />
-    </Background>
+      >
+    </div>
+    <slot />
   </div>
 {/if}
