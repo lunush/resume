cd0256e6e14f32c8f0f3a4e4b1d1ddd35a3adef9
Lunush <76045303+lunush@users.noreply.github.com>
2021-02-11 19:49:40 +0300

    Add a way to return back

--- src/PortfolioWork.svelte
+++ src/PortfolioWork.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
   import { Link } from 'svelte-routing';
 
   export let setFloatingVideoVisibility;
@@ -11,8 +12,21 @@
   });
 </script>
 
-<div class="flex h-screen w-screen justify-center items-center">
-  <div class="flex flex-col w-full h-full max-w-screen-3xl max-h-screen-3xl">
+<div
+  in:fade="{{ delay: 1500, duration: 500 }}"
+  out:fade="{{ duration: 500 }}"
+  class="flex h-screen w-screen justify-center items-center"
+>
+  <div class="absolute p-8 left-0 top-0">
+    <Link
+      to="/"
+      class="text-2xl text-gray-900 no-underline hover:no-underline visited:text-gray-900 hover:text-gray-500 transition duration-500 ease-in-out"
+      >return</Link
+    >
+  </div>
+  <div
+    class="relative flex flex-col w-full h-full max-w-screen-3xl max-h-screen-3xl"
+  >
     <div class="w-full h-full flex">
       <div class="h-full w-full flex flex-col justify-center items-center">
         <h1 class="mb-8 text-5xl text-gray-900 text-center">Notes</h1>
